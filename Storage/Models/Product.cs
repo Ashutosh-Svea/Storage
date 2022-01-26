0d5a89dcaff9f227670911602a7c15f245c9c59c
Ashutosh Ranjan <ashutosh.svea@gmail.com>
2022-01-26 11:00:51 +0100

    Add project files.

---
+++ Storage/Models/Product.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace Storage.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public int Price { get; set; }
+
+        [Display(Name = "Order Date")]
+        [DataType(DataType.Date)]
+        public DateTime  Orderdate { get; set; }
+
+
+//        [BindProperty(SupportsGet = true)]
+        [Required]
+        public string Category { get; set; }
+
+        public string Shelf { get; set; }
+
+        [Range(1, 1000)]
+        public int Count { get; set; }
+
+        public string Description { get; set; }
+
+    }
+
+}
---
+++ Storage/Models/ProductViewModel.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Storage.Models
+{
+    public class ProductViewModel
+    {
+        public string? Name { get; set; }
+
+        public int Price { get; set; }
+
+        public int Count { get; set; }
+
+        public int InventoryValue { get; set; }
+    }
+}
